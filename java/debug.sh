97d23f31c91dcdb43fa1e83b1a548f166b510df0
Jishnu Mohan <jishnu7@gmail.com>
2017-03-06 12:32:41 +0530

    use gradle wrapper in debug script

--- java/debug.sh
+++ java/debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm build/outputs/apk/IndicKeyboard-debug.apk
-gradle --offline assembleDebug
+./gradlew --offline assembleDebug
 adb install -r build/outputs/apk/IndicKeyboard-debug.apk
