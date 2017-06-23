6265505c3af1c468dd176e3d3b9ccaef0b60d9ed
Jishnu Mohan <jishnu7@gmail.com>
2017-06-23 21:33:05 +0530

    use gradle instead of gradlew

--- java/debug.sh
+++ java/debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm build/outputs/apk/IndicKeyboard-debug.apk
-./gradlew --offline assembleDebug
+gradle --offline assembleDebug
 adb install -r build/outputs/apk/IndicKeyboard-debug.apk
