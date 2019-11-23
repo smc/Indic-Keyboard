038471824265311f3ef3f5a2004d5018733b1449
Jishnu Mohan <jishnu7@gmail.com>
2019-11-23 16:54:35 +0530

    use new path for debug

--- java/debug.sh
+++ java/debug.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rm build/outputs/apk/debug/IndicKeyboard-debug.apk
+rm build/outputs/apk/debug/IndicKeyboard-arm64-v8a-debug.apk
 # set up gradle wrapper first
 # gradle wrapper --gradle-version 4.10.1
 ./gradlew --offline assembleDebug
-adb install -r build/outputs/apk/debug/IndicKeyboard-debug.apk
+adb install -r build/outputs/apk/debug/IndicKeyboard-arm64-v8a-debug.apk
+sleep 5
+adb logcat | grep `adb shell ps | grep org.smc.inputmethod.indic | awk '{ print $2; }'`
