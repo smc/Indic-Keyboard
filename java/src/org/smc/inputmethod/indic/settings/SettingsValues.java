251c8f82ab166b973e002df47db7c4a0b3853770
Jishnu Mohan <jishnu7@gmail.com>
2020-04-04 16:38:07 +0530

    remove duplicate variable assignment

--- java/src/org/smc/inputmethod/indic/settings/SettingsValues.java
+++ java/src/org/smc/inputmethod/indic/settings/SettingsValues.java
@@ -184,7 +184,6 @@ public class SettingsValues {
                 && prefs.getBoolean(Settings.PREF_GESTURE_FLOATING_PREVIEW_TEXT, true);
         mAutoCorrectionEnabledPerUserSettings = mAutoCorrectEnabled
                 && !mInputAttributes.mInputTypeNoAutoCorrect;
-        mSuggestionsEnabledPerUserSettings = readSuggestionsEnabled(prefs);
         mSuggestionsEnabledPerUserSettings = !mInputAttributes.mNoLearning
                 && readSuggestionsEnabled(prefs);
         mIsInternal = Settings.isInternal(prefs);
