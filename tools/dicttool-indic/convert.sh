64bf9216a381a7ddae1e047f9d22e9aa19afa7e6
Jishnu Mohan <jishnu7@gmail.com>
2026-06-28 16:06:13 +0530

    remove dictools folder

--- tools/dicttool-indic/convert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for filename in ../*.combined; do
-  IFS=_ read lang <<< `basename $filename`
-  echo "$lang"
-  java -jar dicttool_aosp.jar makedict -s $filename -d main_${lang}.dict
-done
