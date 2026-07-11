5ba2951715dacca064e0ec53e19981e16141bce6
Jishnu Mohan <jishnu7@gmail.com>
2026-07-11 21:34:48 +0530

    Drop clear-history from clipboard settings
    The privacy screen is now the single home for that action.

--- java/src/org/smc/inputmethod/indic/settings/ClipboardSettingsFragment.java
+++ java/src/org/smc/inputmethod/indic/settings/ClipboardSettingsFragment.java
@@ -18,33 +18,12 @@ package org.smc.inputmethod.indic.settings;
 
 import android.os.Bundle;
 
-import androidx.preference.Preference;
-
 import com.android.inputmethod.latin.R;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-
-import org.smc.inputmethod.indic.clipboard.ClipboardHistoryManager;
 
 public final class ClipboardSettingsFragment extends SubScreenFragment {
     @Override
     public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
         addPreferencesFromResource(R.xml.prefs_screen_clipboard);
-
-        final Preference clearHistory = findPreference("pref_clipboard_clear_history");
-        clearHistory.setOnPreferenceClickListener(p -> {
-            confirmClearHistory();
-            return true;
-        });
-    }
-
-    private void confirmClearHistory() {
-        new MaterialAlertDialogBuilder(requireContext())
-                .setTitle(R.string.clipboard_clear_history)
-                .setMessage(R.string.clipboard_clear_history_confirm)
-                .setPositiveButton(R.string.clipboard_clear_all, (d, w) ->
-                        ClipboardHistoryManager.init(requireContext()).clearHistory())
-                .setNegativeButton(android.R.string.cancel, null)
-                .show();
     }
 }
